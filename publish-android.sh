a14372e56287e4e9a86107a693fe34694663f31f
Julien Blancher <julien.blancher@webusco.fr>
2015-10-11 20:10:09 +0200

    [RM] Useless file for public development

--- publish-android.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cordova build --release android
-cd platforms/android/build
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore keystore/intra42.keystore outputs/apk/android-release-unsigned.apk intra42
-cd outputs/apk
-rm intra42.apk
-zipalign -v 4 android-release-unsigned.apk intra42.apk
