e50c64d969c2c7e8001bc7a886edfbc1c5ba69a4
Julien Blancher <julien.blancher@webusco.fr>
2015-10-18 16:03:18 +0200

    [ADD] Skills on dashboard

--- www/js/controllers/dashboard.controller.js
+++ www/js/controllers/dashboard.controller.js
@@ -1,6 +1,8 @@
 angular.module('intra42.controllers')
     .controller('DashboardCtrl', function ($scope, $rootScope, $localStorage, $state, API42Interactions) {
 
+        $scope.skills = $rootScope.Authentication.user.cursus[0].skills;
+
         $scope.refresh = function () {
             $scope.$broadcast('scroll.refreshComplete');
         };
