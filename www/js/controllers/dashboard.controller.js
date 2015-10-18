9da930c48dade5d605ad0c0100321ff063cb7d50
Julien Blancher <julien.blancher@webusco.fr>
2015-10-18 16:12:47 +0200

    [ADD] Current projects

--- www/js/controllers/dashboard.controller.js
+++ www/js/controllers/dashboard.controller.js
@@ -1,7 +1,22 @@
 angular.module('intra42.controllers')
     .controller('DashboardCtrl', function ($scope, $rootScope, $localStorage, $state, API42Interactions) {
 
+        function getProjects() {
+            var userProjects = $rootScope.Authentication.user.cursus[0].projects;
+            var currentProjects = [];
+
+            for (var i = 0; i < userProjects.length; i++) {
+                if (userProjects[i].final_mark === null) {
+                    currentProjects.push(userProjects[i]);
+                }
+            }
+
+            return currentProjects;
+        }
+
         $scope.skills = $rootScope.Authentication.user.cursus[0].skills;
+
+        $scope.projects = getProjects();
 
         $scope.refresh = function () {
             $scope.$broadcast('scroll.refreshComplete');
